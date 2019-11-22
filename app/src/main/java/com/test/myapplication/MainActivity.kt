df54392f194c32e913311809fb8fdb39927bd965
doni.winata <doni.winata@traveloka.com>
2019-11-22 15:02:58 +0700

    dodo

--- app/src/main/java/com/test/myapplication/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -2,11 +2,14 @@ package com.test.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.facebook.stetho.Stetho
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Stetho.initializeWithDefaults(this);
         setContentView(R.layout.activity_main)
 
         val db = WordRoomDatabase.getDatabase(application)
